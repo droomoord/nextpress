acb59b8fb9671aa029f2830f5492c6241d49a1f8
Kris Heijnen <heijnenkris@gmail.com>
2022-04-05 17:39:10 +0200

    delete comment in settings

--- settings.js
+++ settings.js
@@ -3,7 +3,6 @@ const settings = {
   // The page to use as the home page: (required, case-insensitive)
   homePage: "home",
   // The page to use as the posts page: (optional case-insensitive)
-  // postsPage: "posts",
   postsPage: "posts",
   // The revalidation time (in seconds) used by the incremental SSR. If omitted (or set to 0), ISSR will not be used:
   revalidationTime: 5 * 60,
