1fdb874e888eee885fb96846d1b2d33c7dfecf74
Kris Heijnen <heijnenkris@gmail.com>
2022-07-06 16:44:00 +0200

    fix font

--- pages/_document.js
+++ pages/_document.js
@@ -23,12 +23,18 @@ class MyDocument extends Document {
             // as="font"
             // crossOrigin=""
           />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          {/* <link
+            rel="preload"
+            href="/assets/fonts/Saira.ttf"
+            as="Saira"
+            crossOrigin=""
+          /> */}
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Saira:ital,wght@0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
-          ></link>
+          ></link> */}
           {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
