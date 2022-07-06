1ededf385ede1df563b186beaf6b89885ba0e8dc
Kris Heijnen <heijnenkris@gmail.com>
2022-07-06 15:08:37 +0200

    google font

--- pages/_document.js
+++ pages/_document.js
@@ -23,8 +23,8 @@ class MyDocument extends Document {
             // as="font"
             // crossOrigin=""
           />
-          {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
-          {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin /> */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Saira:ital,wght@0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
